63d0366f6ac6d2936c26aa6c0c2029a5f2413b2a
Nick Moriarty <njmoriarty@gmail.com>
2019-06-17 18:42:52 +0100

    Generic container init / teardown

--- fmcore.c
+++ fmcore.c
@@ -18,15 +18,11 @@ int fmch_init(fmcontainer_t *ch /*, patch */) {
 
   size_t n_elements = n_ops + n_adsr + n_ampl;
 
-  /* FIXME: Should not be doing calloc, needs to be sized correctly */
-  ch->p_elements = calloc(n_elements, sizeof(fmel_t*));
-  ch->n_elements = n_elements;
-
-  if(ch->p_elements==0) return -1;
+  if(fmcontainer_init(ch, n_ops + n_adsr + n_ampl)) return -1;
 
   /* Patch does this */
   fmel_t **el = ch->p_elements;
-  fmosc_t* op = calloc(2, sizeof(fmosc_t));
+  fmosc_t* op = calloc(2, sizeof(fmosc_t)); /* FIXME: never freed */
   fmosc_configure(&op[0], 2.0, 0.006, &lfo_osc);
   fmosc_configure(&op[1], 1.0, 0.4, &op[0]);
   el[0] = &op[0];
@@ -37,12 +33,19 @@ int fmch_init(fmcontainer_t *ch /*, patch */) {
   return 0;
 }
 
-void fmch_free(fmcontainer_t *ch) {
-  /* TODO: Free each element */
-  if(ch->p_elements) free(ch->p_elements);
+int fmcontainer_init(fmcontainer_t *co, size_t n_elements) {
+  if(co->p_elements) free(co->p_elements);
+  co->p_elements = calloc(n_elements, sizeof(fmel_t*));
+  co->n_elements = n_elements;
+  return (co->p_elements ? 0 : -1);
+}
 
-  ch->n_elements = 0;
-  ch->p_elements = 0;
+/* NB: Free your own elements */
+void fmcontainer_free(fmcontainer_t *co) {
+  if(co->p_elements) free(co->p_elements);
+
+  co->n_elements = 0;
+  co->p_elements = 0;
 }
 
 void fmch_timestep(fmcontainer_t *ch) {
@@ -73,14 +76,14 @@ void render(float duration, fmcontainer_t *ch) {
 }
 
 int main() {
-  fmcontainer_t clar;
+  fmcontainer_t *clar = calloc(1, sizeof(fmcontainer_t));
   float lfo_rate = 10.0;
 
   fprintf(stderr, "Initializing\n");
   fmosc_init();
 
   // Initialize 3 channels
-  fmch_init(&clar);
+  fmch_init(clar);
 
   fmosc_configure(&lfo_osc, 1.0, 0.0, 0);
   lfo_osc.el.event(&lfo_osc, fmev_freq_change, &lfo_rate);
@@ -92,15 +95,15 @@ int main() {
   const float g4 = 391.9954;
 
   for(int i=0; i<16; ++i) {
-    fmch_event(&clar, fmev_note_on, 0);
-    fmch_event(&clar, fmev_freq_change, &c4);
+    fmch_event(clar, fmev_note_on, 0);
+    fmch_event(clar, fmev_freq_change, &c4);
     //fmel_event(clar->[1], fmev_freq_change, &e4);
     //fmel_event(clar->[2], fmev_freq_change, &g4);
-    render(0.9 * 0.25, &clar);
-    fmch_event(&clar, fmev_note_off, 0);
+    render(0.9 * 0.25, clar);
+    fmch_event(clar, fmev_note_off, 0);
     //fmel_event(&fm_operators[1], fmev_note_off, 0);
     //fmel_event(&fm_operators[2], fmev_note_off, 0);
-    render(0.1 * 0.25, &clar);
+    render(0.1 * 0.25, clar);
   }
 
   fprintf(stderr, "Finished\n");
