080462cde56f6b277630015b8105d8fe83a1d324
Nick Moriarty <njmoriarty@gmail.com>
2019-06-17 20:32:19 +0100

    Add amp module

---
+++ fmamp.h
@@ -0,0 +1,16 @@
+// fmamp.h
+// Summing Amplifier
+
+#include "fmcore.h"
+#include "stddef.h"
+
+typedef struct fmamp_s {
+  fmel_t el;
+  fmel_t **inputs;
+  sample_t *ampl;
+  size_t n_inputs;
+} fmamp_t;
+
+int fmamp_init(fmamp_t *amp, size_t n_inputs);
+void fmamp_connect(fmamp_t *amp, int index, fmel_t *input, sample_t amplitude);
+void fmamp_set_amplitude(fmamp_t *amp, int index, sample_t amplitude);
